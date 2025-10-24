7223e306579bc1358552d057a8a41f06e092679a
Łukasz Michalak <lupinummail@gmail.com>
2025-10-24 09:29:04 +0200

    Initial Commit

---
+++ Awantura o Kase/Awantura_o_KaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Awantura_o_KaseApp.swift
+//  Awantura o Kase
+//
+//  Created by Łukasz Michalak on 24/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Awantura_o_KaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
